76b9fad228e7378fa5424d2039a81f4dd3985929
RicardoU13 <ucanricardo13@gmail.com>
2022-10-27 13:08:42 -0500

    primera actividad videojuegos - API

---
+++ Domain/Modelos/Personaje.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Modelos
+{
+    public class Personaje
+    {
+        [Key]
+        public int PkPersonaje { get; set; }
+        public string Nombre { get; set; }  
+        public string Color  { get; set; }  
+        public string Poder { get; set; }
+
+        [ForeignKey("genero")]
+        public int FkGenero { get; set; }
+        
+        public Genero genero { get; set; }
+
+    }
+}
---
+++ IDYGS102API/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace IDYGS102API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Personaje> personajes { get; set; }
+        public DbSet<Genero> generos { get; set; }
+    }
+}
